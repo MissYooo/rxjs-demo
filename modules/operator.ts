9db0d1476f87fb884c1581654d7394700f3964b7
韩强 <1915217655@qq.com>
2024-08-20 15:12:36 +0800

    feat(pipe): ✨ add  tap、pipe Demo

--- modules/operator.ts
+++ modules/operator.ts
@@ -1,7 +1,10 @@
-import { of, map, first, interval, take, filter } from "rxjs";
+import { of, map, first, interval, take, filter, delay, tap } from "rxjs";
 
 // of(1, 2, 3).pipe(map(x => x * 2)).subscribe(console.log)
 // of(1, 2, 3).pipe(first()).subscribe(console.log)
 // of(1, 2, 3).pipe(map(x => x * 2), first()).subscribe(console.log)
 
-interval(500).pipe(map(x => x + 1), take(10)).subscribe(console.log)
+// interval(500).pipe(map(x => x + 1), take(10)).subscribe(console.log)
+
+let time = Date.now()
+of('hello').pipe(tap(() => { time = Date.now() }), delay(1000), tap(() => { console.log(`After ${Date.now() - time}`) })).subscribe(console.log)
